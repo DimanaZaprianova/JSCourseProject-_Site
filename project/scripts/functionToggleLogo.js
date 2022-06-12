e90678f59ec5f7499aa9ed49b699a4e4be5fea91
DimanaZaprianova <105113180+DimanaZaprianova@users.noreply.github.com>
2022-06-12 10:15:02 +0300

    Add files via upload

--- project/scripts/functionToggleLogo.js
+++ project/scripts/functionToggleLogo.js
@@ -1,9 +1,10 @@
 // function toggle logo which changes the logo image by clicking the button with id toggleLogo
-document.getElementById("toggleLogo").onclick = function() {
-    var logo = document.getElementsByTagName("img")[0];
+const toggleLogo = document.querySelector("#toggleLogo")
+toggleLogo.addEventListener('click', function() {
+    var logo = document.querySelector("#img");
     if (logo.src.match("logo.jpg")) {
         logo.src = "images/logo2.jpg";
     } else {
         logo.src = "images/logo.jpg";
-    }
-};
+    };
+});
